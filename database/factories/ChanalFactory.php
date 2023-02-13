ffc5817a5e9c75ab28ba584b93418112c0c8f3f3
Osama Al-mamari <yemencoder@gmail.com>
2023-02-14 00:18:23 +0300

    factory

--- database/factories/ChanalFactory.php
+++ database/factories/ChanalFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Chanal;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +15,15 @@ class ChanalFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+
+
+    protected $model = Chanal::class;
+
     public function definition()
     {
         return [
-            //
+            'name' => fake()->name(),
         ];
     }
 }
