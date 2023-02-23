b8a10899fac398b6d91f000606724af1e8ec5244
Osama Al-mamari <yemencoder@gmail.com>
2023-02-23 22:02:24 +0300

    install  laravel/breez
    composer require laravel/breeze --dev
    
    php artisan breeze:install
    
    php artisan migrate
    npm install
    npm run dev

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PaymentController;
-use App\Jobs\TestJob;
-use App\Models\Task;
+use App\Http\Controllers\ProfileController;
 use App\PostCard;
 use App\PostCardSendingService;
 use Illuminate\Support\Facades\Route;
@@ -12,21 +11,35 @@ use Illuminate\Support\LazyCollection;
 use  Illuminate\Support\Str;
 use  Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Queue;
-
+use App\Jobs\TestJob;
+use App\Models\Task;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
 
 Route::get('pay', [PaymentController::class, 'store']);
@@ -112,4 +125,4 @@ Route::get('q3', function () {
     TestJob::dispatch($task);
 });
 
-Route::resource("products",\App\Http\Controllers\ProductController::class);
+Route::resource("products", \App\Http\Controllers\ProductController::class);
